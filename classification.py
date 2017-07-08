bf9ed62e77be4795b2b4e9cfbab26cf0c3e1104e
Tadewos Somano <tadewos85@gmail.com>
2017-07-08 12:16:28 +0200

    Update classification.py

--- classification.py
+++ classification.py
@@ -12,20 +12,6 @@
 '''
 Data Scientist for  Day - Codelab source code - classification.py
 
-Copyright (C) 2013  Ferrari Alessandro
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.o
 '''
 
 from sklearn.cross_validation import train_test_split
